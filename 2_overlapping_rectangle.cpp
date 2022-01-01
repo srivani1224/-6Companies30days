5f5304f20c137e3b596242424d136a733d0ac201
srivani1224 <59949479+srivani1224@users.noreply.github.com>
2022-01-01 16:35:16 +0530

    Create 2_overlapping_rectangle.cpp

---
+++ 2_overlapping_rectangle.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        if( ((R2[1] > L1[1]) || (R1[1] > L2[1])) || ((L2[0] > R1[0]) || (L1[0] > R2[0])) )
+            return 0;
+        return 1;
+    }
+};

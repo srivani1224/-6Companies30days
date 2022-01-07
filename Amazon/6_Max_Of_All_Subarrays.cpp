ce4d7cde31b3839f86d904eb4edd211937131c4a
srivani1224 <59949479+srivani1224@users.noreply.github.com>
2022-01-07 18:58:13 +0530

    Amazon

---
+++ Amazon/6_Max_Of_All_Subarrays.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *v, int n, int k)
+    {
+        deque<int> dq;
+        vector<int> ans;
+        dq.push_back(0);
+        for(int i=1;i<k;i++)
+        {
+            while(!dq.empty() && v[i] >= v[dq.back()])
+                dq.pop_back();
+            dq.push_back(i);
+        }
+        ans.push_back(v[dq.front()]);
+        for(int i=k;i<n;i++)
+        {
+            while(!dq.empty() && dq.front()<=i-k)
+                dq.pop_front();
+            while(!dq.empty() && v[i] >= v[dq.back()])
+                dq.pop_back();
+            dq.push_back(i);
+            ans.push_back(v[dq.front()]);
+        }
+        return ans;
+    }
+};

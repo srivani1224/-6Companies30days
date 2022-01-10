10026bc0fd43aa3b03abd5513511fb55ee8ae29c
srivani1224 <59949479+srivani1224@users.noreply.github.com>
2022-01-10 22:10:14 +0530

    Amazon

--- Amazon/4_Brackets_Matrix.cpp
+++ Amazon/4_Brackets_Matrix.cpp
@@ -1,19 +1,19 @@
 class Solution{
 public: 
     string s;
-    void printParenthesis(int i, int j, int n, int *a, char &name){
+    void printParenthesis(int i, int j, int a[][505], char &name){
     	if (i == j){
     		s += name;
     		name++;
     		return;
     	}
     	s += '(';
-    	printParenthesis(i, *((a+i*n)+j), n, a, name);
-    	printParenthesis(*((a+i*n)+j) + 1, j, n, a, name);
+    	printParenthesis(i, a[i][j], a, name);
+    	printParenthesis(a[i][j] + 1, j, a, name);
     	s += ')';
     }
     string matrixChainOrder(int p[], int n){
-        int c[n-1][n-1]={0}, a[n-1][n-1]={0};
+        int c[n-1][n-1]={0}, a[505][505]{0};
         for(int d=1;d<n-1;d++)
         {
             for(int i=0,j=i+d;i<n-1 && j<n-1;i++,j++)
@@ -33,7 +33,7 @@ public:
             }
         }
         char name = 'A';
-    	printParenthesis(0, n-2, n-1, (int *)a, name);
+    	printParenthesis(0, n-2, a, name);
     	return s;
     }
 };

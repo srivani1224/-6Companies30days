fc1f415c5cdd19df9fe4aff817b9318d44418e96
srivani1224 <59949479+srivani1224@users.noreply.github.com>
2022-01-14 18:50:26 +0530

    Microsoft

---
+++ Microsoft/10_Stickler_Thief.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+   
+    int FindMaxSum(int a[], int n)
+    {
+        int dp[n];
+        if(n==1)
+            return a[0];
+        if(n==2)
+            return max(a[0],a[1]);
+        dp[0] = a[0];
+        dp[1] = max(a[0], a[1]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = max(a[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
+    }
+};

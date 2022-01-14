c76d73b2a6249ccb1d0805dba1372db029ab7422
srivani1224 <59949479+srivani1224@users.noreply.github.com>
2022-01-14 17:55:15 +0530

    Microsoft

--- Microsoft/8_Connect_Nodes_Same_Level.cpp
+++ Microsoft/8_Connect_Nodes_Same_Level.cpp
@@ -6,6 +6,37 @@ class Solution
     {
        queue<Node*> q;
        q.push(root);
+       while(!q.empty())
+       {
+           int n = q.size();
+           for(int i=0;i<n;i++)
+           {
+                Node* temp  = q.front();
+                q.pop();
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+                if(i+1 == n)
+                    temp -> nextRight = NULL;
+                else
+                    temp -> nextRight = q.front();
+           }
+       }
+    }    
+      
+};
+
+/* Method 2
+
+class Solution
+{
+    public:
+    //Function to connect nodes at same level.
+    void connect(Node *root)
+    {
+       queue<Node*> q;
+       q.push(root);
        Node *prev = NULL;
        q.push(NULL);
        while(!q.empty())
@@ -31,3 +62,5 @@ class Solution
     }    
       
 };
+
+*/

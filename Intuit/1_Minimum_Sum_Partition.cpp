b0636d3c3135e33df7df0310c8234a318618e045
srivani1224 <59949479+srivani1224@users.noreply.github.com>
2022-01-25 16:24:29 +0530

    Intuit

---
+++ Intuit/1_Minimum_Sum_Partition.cpp
@@ -0,0 +1,43 @@
+class Solution{
+
+  public:
+    
+	int minDifference(int a[], int n)  
+	{ 
+	    int s=0;
+	    for(int i=0;i<n;i++)
+	        s+=a[i];
+	    bool dp[n+1][s+1];
+	    for(int i=0;i<=n;i++)
+	    {
+	        for(int j=0;j<=s;j++)
+	        {
+	            if(i==0)
+	                dp[i][j] = false;
+	            if(j==0)
+	                dp[i][j] = true;
+	            else if(i!=0 && j!=0)
+	            {
+	                if(a[i-1] <= j)
+	                {
+	                    dp[i][j] = (dp[i-1][j] || dp[i-1][j-a[i-1]]);
+	                }
+	                else
+	                {
+	                    dp[i][j] = dp[i-1][j];
+	                }
+	            }
+	        }
+	    }
+	    int ans = s;
+	    for(int i=(s/2);i>=0;i--)
+	    {
+	        if(dp[n][i])
+	        {
+	            ans = abs(s - (2*i));
+	            break;
+	        }
+	    }
+	    return ans;
+	} 
+};
